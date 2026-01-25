e69c35df81e1f9f56c73628e63b39486645466f7
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2026-01-25 18:00:16 +0500

    two methods were added one for inviting admin by super admin and one for verifing him

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,5 +3,11 @@ const authorize = require('../middlewware/authorizationMiddleware.js');
 const router = express.Router();
 const { fetchAllReports } = require('../controllers/admin/fetchAllReports.js');
 const { protectedRoute } = require('../middlewware/verifyTokenMiddleware.js');
+const { inviteAdmin } = require('../controllers/admin/inviteAdmin.js');
+const { verifyAdminRole } = require('../controllers/admin/verifyAdminRole.js');
+
 router.get('/get/reports', protectedRoute, authorize(['admin', 'super_admin']), fetchAllReports);
+
+router.post('/invite/admin', protectedRoute, authorize(['admin']), inviteAdmin);
+router.post('/verify/admin-role', verifyAdminRole);
 module.exports = router;
