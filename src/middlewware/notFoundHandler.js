9324aa00c506c2aadea6403162b629c46430e5f8
Professor Irfan <irfanprofessor60@gmail.com>
2026-02-02 08:21:56 -0500

    fixed the status code and the ugly html response

--- src/middlewware/notFoundHandler.js
+++ src/middlewware/notFoundHandler.js
@@ -1,7 +1,7 @@
 const notFoundHandler = (req, res, next) => {
 
     const error = new Error(`Route Not Found - ${req.originalUrl}`);
-    error.status = 404;
+    error.statusCode  = 404;
     next(error);
 };
 
