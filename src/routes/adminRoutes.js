5c1f5b8335e2f3794b044adff175813f3ee5f287
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2026-02-10 11:24:34 +0500

    add analytics route for admins and super admins for instant resposne

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -5,8 +5,11 @@ const { fetchAllReports } = require('../controllers/admin/fetchAllReports.js');
 const { protectedRoute } = require('../middlewware/verifyTokenMiddleware.js');
 const { inviteAdmin } = require('../controllers/admin/inviteAdmin.js');
 const { verifyAdminRole } = require('../controllers/admin/verifyAdminRole.js');
+const { fetchAdminAnalytics } = require('../controllers/admin/fetchAdminAnalytics.js');
 
 router.get('/get/reports', protectedRoute, authorize(['admin', 'super_admin']), fetchAllReports);
+router.get('/get/analytics', protectedRoute, authorize(['admin', 'super_admin']), fetchAdminAnalytics);
+
 
 router.post('/invite/admin', protectedRoute, authorize(['admin']), inviteAdmin);
 router.post('/verify/admin-role', verifyAdminRole);
