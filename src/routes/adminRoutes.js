373485ca351c4a567104e1ffd3afe94dbff67ddb
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2026-02-23 02:24:52 +0500

    added two routes to this file

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -5,10 +5,15 @@ const { fetchAllReports } = require('../controllers/admin/fetchAllReports.js');
 const { protectedRoute } = require('../middlewware/verifyTokenMiddleware.js');
 const { inviteAdmin } = require('../controllers/admin/inviteAdmin.js');
 const { verifyAdminRole } = require('../controllers/admin/verifyAdminRole.js');
+const { generatePdf } = require('../controllers/admin/generatePdf.js');
 const { fetchAdminAnalytics } = require('../controllers/admin/fetchAdminAnalytics.js');
+const { fetchReport } = require('../controllers/admin/fetchSingleReport')
 
+router.get('/get/report', protectedRoute, authorize(['admin', 'super_admin']), fetchReport);
 router.get('/get/reports', protectedRoute, authorize(['admin', 'super_admin']), fetchAllReports);
 router.get('/get/analytics', protectedRoute, authorize(['admin', 'super_admin']), fetchAdminAnalytics);
+router.get('/download-pdf', protectedRoute, authorize(['admin', 'super_admin']), generatePdf);
+
 
 
 router.post('/invite/admin', protectedRoute, authorize(['admin']), inviteAdmin);
