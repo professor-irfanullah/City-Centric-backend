56e44c91aeec06e87b0a88b8ba2be36605afeff7
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2026-03-07 02:41:58 +0500

    added get admins and verify admin roles routes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -7,15 +7,17 @@ const { inviteAdmin } = require('../controllers/admin/inviteAdmin.js');
 const { verifyAdminRole } = require('../controllers/admin/verifyAdminRole.js');
 const { generatePdf } = require('../controllers/admin/generatePdf.js');
 const { fetchAdminAnalytics } = require('../controllers/admin/fetchAdminAnalytics.js');
-const { fetchReport } = require('../controllers/admin/fetchSingleReport')
+const { fetchReport } = require('../controllers/admin/fetchSingleReport');
+const { fetchAdminsData } = require('../controllers/admin/fetchAdminsAboutInfo.js');
+const { verificationEmailLimiter } = require('../middlewware/rateLimiter')
 
 router.get('/get/report', protectedRoute, authorize(['admin', 'super_admin']), fetchReport);
 router.get('/get/reports', protectedRoute, authorize(['admin', 'super_admin']), fetchAllReports);
 router.get('/get/analytics', protectedRoute, authorize(['admin', 'super_admin']), fetchAdminAnalytics);
 router.get('/download-pdf', protectedRoute, authorize(['admin', 'super_admin']), generatePdf);
-
+router.get('/get/admins', protectedRoute, authorize(['admin', 'super_admin']), fetchAdminsData)
 
 
 router.post('/invite/admin', protectedRoute, authorize(['admin']), inviteAdmin);
-router.post('/verify/admin-role', verifyAdminRole);
+router.post('/verify/admin-role', verificationEmailLimiter, verifyAdminRole);
 module.exports = router;
