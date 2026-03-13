c0fa8265f3bc2ce5afd407793e26550e0970657d
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2026-03-13 19:23:50 +0500

    used host port in server.js file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./src/app.js')
 const PORT = process.env.SERVER_PORT
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is listening on port ${PORT}`);
 });
